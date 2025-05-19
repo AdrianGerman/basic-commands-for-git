8bd7faf9d7d0082553d81544abd09ae115e80b7f
AdrianGerman <adrian.german1019@gmail.com>
2025-05-19 09:47:31 -0600

    init project git commands

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-6xl font-bold">Basic Commands For GIT</h1>
+    </main>
+  )
+}
+
+export default App
