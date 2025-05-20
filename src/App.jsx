bc2174f0b080b5f42d6b3b4f5d96ef0908c5393e
AdrianGerman <adrian.german1019@gmail.com>
2025-05-20 09:35:43 -0600

    add background style

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { Content } from "./components/Content"
 
 function App() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-[#1e1e1e]">
-      <Content />
+    <main class="relative inset-0 -z-10 min-h-screen w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+      <div className="flex flex-col items-center justify-center">
+        <Content />
+      </div>
     </main>
   )
 }
