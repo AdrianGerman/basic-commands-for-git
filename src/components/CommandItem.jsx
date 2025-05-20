7bb6a0eed3d1d74295c0804f156c744c2d09e3f9
AdrianGerman <adrian.german1019@gmail.com>
2025-05-20 09:24:53 -0600

    add styles for command item

--- src/components/CommandItem.jsx
+++ src/components/CommandItem.jsx
@@ -1,6 +1,10 @@
 export const CommandItem = ({ command, description }) => (
-  <div className="bg-gray-700 border-2 border-black p-2 rounded-lg shadow pixel-border">
-    <p className="text-green-400 font-mono text-sm">{command}</p>
-    <p className="text-gray-300 text-xs">{description}</p>
+  <div className="bg-gray-700 p-2 rounded-none shadow-[4px_4px_0px_0px_#000] outline-2 outline-black transition-transform duration-300 hover:scale-105 cursor-pointer">
+    <p className="text-green-400 font-mono text-sm before:content-['$'] before:mr-1 before:text-gray-500 bg-black py-1 px-2">
+      {command}
+    </p>
+    <p className="text-gray-400 text-xs before:content-['#'] before:mr-1 mt-2">
+      {description}
+    </p>
   </div>
 )
